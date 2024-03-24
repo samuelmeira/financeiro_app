c714900dc468e92fb6bf84df87e76b0d7bfd0fe0
Samuel Meira <slmeira.dev@gmail.com>
2024-03-24 19:44:12 -0300

    fix: adjust database name

--- config/dev.exs
+++ config/dev.exs
@@ -5,7 +5,7 @@ config :financeiro_app, FinanceiroApp.Repo,
   username: "postgres",
   password: "postgres",
   hostname: "localhost",
-  database: "financeiro_app_dev",
+  database: "financial_db",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
